64571a877540c71808dfe7eddca97a7774c45804
Ivan <ivancordeiro@yahoo.com.br>
2018-09-27 07:23:36 -0300

    'up'

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -916,7 +916,7 @@ if (document.location.hostname == "localhost") {
 	
 	
 	$(".bt_voltar_new_pergunta3").off().click(function() {
-		 mainView.router.loadPage('spotcheck-justificar.html');														  
+		// mainView.router.loadPage('spotcheck-justificar.html');														  
 
     });	
 	
